7b6219ce18e5037f8e8e51c4679717bb1840fdb7
MobiCartEngineering <sumeet.kumar@SumeetKumar-2.local>
2013-07-16 15:40:38 +0530

    Update Zooz Sdk and remove UDID
    Update Zooz Sdk and remove UDID issue

--- MobiCart_iPhone/UserDetails.h
+++ MobiCart_iPhone/UserDetails.h
@@ -2,7 +2,7 @@
  *  UserDetails.h
  *  Mobicart
  *
- *  Created by Mobicart on 15/07/13.
+ *  Created by Mobicart on 15/07/13.Update Zooz Sdk and remove UDID
  *  Copyright 2011Mobicart. All rights reserved.
  *
  */
